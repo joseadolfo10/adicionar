2ed8516f1b039d61ffb5b6689766fb58848643c3
joseadolfo10 <139913282+joseadolfo10@users.noreply.github.com>
2023-10-17 09:28:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let contador = 3
+function js_add_rows() {
+    let input = document.querySelector('#txtNumRows').value;
+    const template = document.querySelector("#template");
+    if (filterInt(input) == 's'){
+        document.querySelector('#error').innerHTML = 'Só aceitamos valores numéricos.'
+    }
+    for (let i = 0; i < input; i++) {
+        document.querySelector('#sampleTable').innerHTML += template.innerHTML = `<td>Row${contador} cell1</td>` + `<td>Row${contador} cell2</td>`;
+        contador++
+    }
+}
+filterInt = function (value) {
+    if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value)) return Number(value);
+    return 's';
+  };
